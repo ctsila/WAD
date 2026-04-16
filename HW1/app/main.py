709a4d79765572d6cc9acf7f74a1dfc2ad3d669c
Anastasiia Sila <sila_as@itmo.ru>
2026-04-16 23:55:13 +0300

    Update main.py

--- HW1/app/main.py
+++ HW1/app/main.py
@@ -8,6 +8,15 @@ from app.controllers.message_controller import router as message_router
 from app.database import AsyncSessionLocal
 from app.redis_client import get_redis
 
+from llama_cpp import Llama
+from huggingface_hub import hf_hub_download
+import os
+import shutil
+
+
+HF_REPO_ID = "Jackrong/Qwen3.5-4B-Claude-4.6-Opus-Reasoning-Distilled-GGUF"
+HF_FILENAME = "Qwen3.5-4B.Q2_K.gguf"
+
 
 def create_app() -> FastAPI:
     app = FastAPI(title="LLM Chat App")
@@ -19,6 +28,34 @@ def create_app() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    @app.on_event("startup")
+    async def load_llm():
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        target_model_path = os.path.join(base_dir, "model.gguf")
+
+        if not os.path.exists(target_model_path):
+            print("model.gguf not found, downloading from Hugging Face...")
+            downloaded_path = hf_hub_download(
+                repo_id=HF_REPO_ID,
+                filename=HF_FILENAME,
+                local_dir=base_dir,
+                local_dir_use_symlinks=False,
+            )
+
+            if downloaded_path != target_model_path:
+                shutil.copyfile(downloaded_path, target_model_path)
+
+            print(f"Model saved to: {target_model_path}")
+
+        if not hasattr(app.state, "llm"):
+            print("Loading GGUF model...")
+            app.state.llm = Llama(
+                model_path=target_model_path,
+                n_ctx=4096,
+                n_threads=4,
+            )
+            print("Model loaded")
 
     app.include_router(auth_router, prefix="", tags=["auth"])
     app.include_router(chat_router, prefix="", tags=["chats"])
